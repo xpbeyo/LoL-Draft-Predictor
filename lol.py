cba1809407172b2335904f3ce9cb4a5e209ca53e
Ramzi Dajani <ramzi.r.dajani@gmail.com>
2021-04-16 22:23:17 +0300

    Added base model

--- lol.py
+++ lol.py
@@ -8,7 +8,56 @@ import torch.utils.data
 import numpy as np
 import torch
 
-def train():
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        """ Initialize a class AutoEncoder.
+        :param num_question: int
+        :param k: int
+        """
+        super(AutoEncoder, self).__init__()
+
+        # The number of hidden units for each layer
+        layer1 = 5
+        # = 150
+
+        # Define linear functions.
+        self.g = nn.Linear(10, layer1)
+        self.h = nn.Linear(layer1, 1)
+        self.s = nn.Sigmoid()
+        self.r = nn.ReLU()
+
+    def get_weight_norm(self):
+        """ Return ||W^1|| + ||W^2||.
+        :return: float
+        """
+        g_w_norm = torch.norm(self.g.weight, 2)
+        h_w_norm = torch.norm(self.h.weight, 2)
+        return g_w_norm + h_w_norm
+
+    def forward(self, inputs):
+        """ Return a forward pass given inputs.
+        :param inputs: user vector.
+        :return: user vector.
+        """
+        out = self.g(inputs)
+        out = self.s(out)
+        out = self.g1(out)
+        out = self.r(out)
+        out = self.g2(out)
+        out = self.s(out)
+        out = self.h(out)
+        out = self.r(out)
+        out = self.h1(out)
+        out = self.s(out)
+        out = self.h2(out)
+
+        return out
+
+
+def train(model, num_epoch, lr, lamb):
+    model.train()
+
+    for epoch in range(0, num_epoch):
     return
 
 def load_data(path=""):
@@ -17,12 +66,13 @@ def load_data(path=""):
 def main():
     data = load_data()
 
-    # Set optimization hyperparameters.
+    model = AutoEncoder()
+    # Optimization hyperparameters.
     lr = None
     num_epoch = None
     lamb = None
 
-    train()
+    train(model, num_epoch, lr, lamb)
 
 
 if __name__ == "__main__":
