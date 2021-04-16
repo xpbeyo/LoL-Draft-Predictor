c9e96f66348f20f8a25cd7688a7381370f69d65b
Ramzi Dajani <ramzi.r.dajani@gmail.com>
2021-04-16 21:22:52 +0300

    Base Code

---
+++ lol.py
@@ -0,0 +1,29 @@
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data
+
+import numpy as np
+import torch
+
+def load_data(path=""):
+    return
+
+def main():
+    data = load_data()
+
+    # Set optimization hyperparameters.
+    lr = None
+    num_epoch = None
+    lamb = None
+
+    train()
+    #####################################################################
+    #                       END OF YOUR CODE                            #
+    #####################################################################
+
+
+if __name__ == "__main__":
+    main()
